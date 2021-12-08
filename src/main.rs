566ac5b3085a4ddc48a874db1ed639e600c8db54
Simon Pertoft <simon@spectral.ninja>
2021-12-08 21:29:01 +0100

    fixed mistyping for main file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod day_1;
-use crate::day_1::run_part_1;
-use crate::day_1::run_part_2;
+mod day_3;
+use crate::day_3::run_part_1;
+use crate::day_3::run_part_2;
 
 fn main() {
     println!("Answer for part 1: {:?}", run_part_1());
-    println!("Answer for part 1: {:?}", run_part_2());
+    println!("Answer for part 2: {:?}", run_part_2());
 }
