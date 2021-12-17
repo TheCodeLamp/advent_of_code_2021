0e5b2a15d9718bf4bb205e5d820cdf8b3335899f
Simon Pertoft <simon@spectral.ninja>
2021-12-17 19:05:37 +0100

    Day 6 INIT

---
+++ src/day_6/mod.rs
@@ -0,0 +1,7 @@
+pub fn run_part_1() -> Option<usize>{
+    None
+}
+
+pub fn run_part_2() -> Option<usize>{
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod day_5;
-use crate::day_5::run_part_1;
-use crate::day_5::run_part_2;
+mod day_6;
+use crate::day_6::run_part_1;
+use crate::day_6::run_part_2;
 
 fn main() {
     println!("Answer for part 1: {:?}", run_part_1());
