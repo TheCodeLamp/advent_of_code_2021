fb2577dbe3d864251e32d9911772a7ff3b5bbd6e
Simon Pertoft <simon@spectral.ninja>
2021-12-17 20:13:43 +0100

    Day 8 INIT

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod day_7;
-use crate::day_7::run_part_1;
-use crate::day_7::run_part_2;
+mod day_8;
+use crate::day_8::run_part_1;
+use crate::day_8::run_part_2;
 
 fn main() {
     println!("Answer for part 1: {:?}", run_part_1());
