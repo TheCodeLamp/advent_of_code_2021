8e18d193360723fb88d60bbf5bcd15ff3bc1a4ed
Simon Pertoft <simon@spectral.ninja>
2021-12-17 20:08:54 +0100

    Day 7 done

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -9,11 +9,11 @@ pub fn run_part_1() -> Option<usize> {
 
     let mut max_fuel = usize::MAX;
 
-    for dif in input.iter() {
+    for dif in 0..=*input.last().unwrap() {
         let mut fuel_usage = 0;
-        
+
         for num in input.iter() {
-            fuel_usage += (*num as i32 - *dif as i32).abs() as usize;
+            fuel_usage += (*num as i32 - dif as i32).abs() as usize;
         }
 
         if fuel_usage < max_fuel {
@@ -22,9 +22,30 @@ pub fn run_part_1() -> Option<usize> {
     }
 
     Some(max_fuel)
-
-    
 }
 pub fn run_part_2() -> Option<usize> {
-    None
+    let mut input: Vec<usize> = include_str!("input")
+        .trim()
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    input.sort_unstable();
+
+    let mut max_fuel = usize::MAX;
+
+    for dif in 0..=*input.last().unwrap() {
+        let mut fuel_usage = 0;
+
+        for num in input.iter() {
+            let n = (*num as i32 - dif as i32).abs();
+            fuel_usage += (n*(n+1)/2) as usize;
+        }
+
+        if fuel_usage < max_fuel {
+            max_fuel = fuel_usage;
+        }
+    }
+
+    Some(max_fuel)
 }
