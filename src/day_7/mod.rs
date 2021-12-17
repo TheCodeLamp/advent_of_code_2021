5c5a5aac0afb65344b4423a1a6ac4cb9d2ffdadc
Simon Pertoft <simon@spectral.ninja>
2021-12-17 20:04:09 +0100

    Day 7 part 1 done

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,2 +1,30 @@
-pub fn run_part_1() -> Option<usize> {None}
-pub fn run_part_2() -> Option<usize> {None}
+pub fn run_part_1() -> Option<usize> {
+    let mut input: Vec<usize> = include_str!("input")
+        .trim()
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+
+    input.sort_unstable();
+
+    let mut max_fuel = usize::MAX;
+
+    for dif in input.iter() {
+        let mut fuel_usage = 0;
+        
+        for num in input.iter() {
+            fuel_usage += (*num as i32 - *dif as i32).abs() as usize;
+        }
+
+        if fuel_usage < max_fuel {
+            max_fuel = fuel_usage;
+        }
+    }
+
+    Some(max_fuel)
+
+    
+}
+pub fn run_part_2() -> Option<usize> {
+    None
+}
